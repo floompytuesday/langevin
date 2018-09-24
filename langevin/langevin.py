fa96441310225d5040be93437f365e729d4dc5b3
unknown <skirschn@u.rochester.edu>
2018-09-24 14:07:09 -0400

    added parser and first pass at step

--- langevin/langevin.py
+++ langevin/langevin.py
@@ -1,3 +1,35 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+import argparse 
+import numpy as np
+
+
+def parse_arguements():
+    '''Get arguements from the command line'''
+    parser=argparse.ArgumentParser(description='Langevin Dynamics in Python')
+    parser.add_argument('--initial_position', type=float, default=1)
+    parser.add_argument('--initial_velocity', type=float, default=1)
+    parser.add_argument('--temperature', type=float, default=10)
+    parser.add_argument('--damping_coefficient', type=float, default=1)
+    parser.add_argument('--time_step', type=float, default=1)
+    parser.add_argument('--total_time', type=float, default=10)
+    
+    return parser.parse_args()
+
+args=parse_arguments()
+
+velocity_array=[args.initial_velocity]
+position_array=[args.initial_position]
+time_array=[0]
+
+
+
+def step(xi,vi,args):
+    #calculate one timestep using eulers method
+    drag=-1*args.damping_coefficient*args.inital_velocity
+    random=np.random.normal(0, 2*args.temperature*args.damping_coefficient)
+    force=drag+random
+    vj=vi+force*args.time_step
+    xj=xi+vi*args.time_step
+    return xj,vj
